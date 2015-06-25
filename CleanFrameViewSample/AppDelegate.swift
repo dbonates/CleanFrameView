804d9fffc7dd74f28b1f8587cb674459d6344720
Dmitry Nikolaev <mail@nikolaeu.com>
2015-06-25 14:22:23 +0300

    Fix source sign

--- CleanFrameViewSample/AppDelegate.swift
+++ CleanFrameViewSample/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  CleanFrameViewSample
 //
-//  Created by Дмитрий Николаев on 21.03.15.
+//  Created by Dmitry Nikolaev on 21.03.15.
 //  Copyright (c) 2015 Dmitry Nikolaev. All rights reserved.
 //
 
